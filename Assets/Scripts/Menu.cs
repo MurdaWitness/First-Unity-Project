58bf74b9360bc7ee3189fc23282386b395243167
MurdaWitness <gavrishin.a@yandex.ru>
2021-03-14 00:46:25 +0300

    Старт проекта

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class Menu : MonoBehaviour
+{
+    public Button playBtn;
+    public Button exitBtn;
+
+    void Start()
+    {
+        playBtn.onClick.AddListener(StartGame);
+        exitBtn.onClick.AddListener(ExitGame);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Осуществляю выход");
+    }
+}
